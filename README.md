94cebf71a25b477bab6eb91d3a0a7dccaef413ca
Andrew Doble <andrew.l.doble@gmail.com>
2022-09-11 19:32:31 +0200

    New documentation

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # wav-gen
 
-### Introduction
 
 A utility program to generate wave forms as a `wav` file.
 
 
-### Example Usage
+## Example Usage
+
+### Sine wave
 
 To generate a **sine wave** with:
 - a frequency of 643 hertz
@@ -16,6 +17,9 @@ To generate a **sine wave** with:
 ```console
  wav-gen sine --frequency 643 --duration 3 sine.wav
 ```
+
+### Sweeping Sine Wave
+
 To generate a sine wave that:
 - **sweeps** between the frequencies of 300 hertz and 1000 hertz
 - has a duration of 5 seconds
@@ -24,7 +28,26 @@ To generate a sine wave that:
 ```console
 wav-gen sweep --start 300  --finish 1000 --duration 5 sweep.wav
 ```
+### Harmonics
 
+To generate a wave that has a set of harmonics first define the harmonics using a csv file (for example `harmonics.csv`):
+
+```
+frequency,amplitude
+500.0 , 0.3
+700.0 , 0.2
+750.0 , 0.1
+```
+This specifies a wave with harmonics at 500Hz, 700Hz and 750Hz with respective amplitudes 0.3, 0.2 and 0.1.
+The amplitudes will be normalised.
+
+Then use:
+
+```console
+wav-gen -m harmonics.csv output_wave_file.wav
+```
+
+## More options
 For more options use:
 
 ```console
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-//! ## Introduction
-//!
+//! 
 //! A utility program to generate wave forms as a `wav` file.
 //!
 //!
-//! ## Example Usage
+//! # Example Usage
+//! 
+//! ## Sine wave
 //!
 //! To generate a **sine wave** with:
 //! - a frequency of 643 hertz
@@ -14,6 +15,9 @@
 //! ```console
 //!  wav-gen sine --frequency 643 --duration 3 sine.wav
 //! ```
+//! 
+//! ## Sweeping Sine Wave
+//! 
 //! To generate a sine wave that:
 //! - **sweeps** between the frequencies of 300 hertz and 1000 hertz
 //! - has a duration of 5 seconds
@@ -22,7 +26,26 @@
 //! ```console
 //! wav-gen sweep --start 300  --finish 1000 --duration 5 sweep.wav
 //! ```
-//!
+//! ## Harmonics
+//! 
+//! To generate a wave that has a set of harmonics first define the harmonics using a csv file (for example `harmonics.csv`):
+//! 
+//! ```text
+//! frequency,amplitude
+//! 500.0 , 0.3
+//! 700.0 , 0.2
+//! 750.0 , 0.1
+//! ```
+//! This specifies a wave with harmonics at 500Hz, 700Hz and 750Hz with respective amplitudes 0.3, 0.2 and 0.1. 
+//! The amplitudes will be normalised.
+//! 
+//! Then use:
+//! 
+//! ```console
+//! wav-gen -m harmonics.csv output_wave_file.wav
+//! ```
+//! 
+//! # More options
 //! For more options use:
 //!
 //! ```console
